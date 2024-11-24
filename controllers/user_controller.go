cf9a86212ecd259aad06cb0c1e137dc5a5644bf2
2deadmen <kartikhegde.2002@gmail.com>
2024-11-24 14:03:13 +0530

    swagger configured

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -9,7 +9,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Get all users
+// GetUsers godoc
+// @Summary Get all users
+// @Description Fetch all users from the database
+// @Tags Users
+// @Produce json
+// @Success 200 {array} models.User
+// @Failure 500 {object} map[string]string
+// @Router /users [get]
 func GetUsers(c *gin.Context) {
 	users, err := models.GetAllUsers()
 	if err != nil {
@@ -19,7 +26,17 @@ func GetUsers(c *gin.Context) {
 	utils.RespondJSON(c, http.StatusOK, users)
 }
 
-// Create a new user
+// CreateUser godoc
+// @Summary Create a new user
+// @Description Add a new user to the database
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param user body models.User true "User Data"
+// @Success 201 {object} models.User
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /users [post]
 func CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
--- routes/routes.go
+++ routes/routes.go
@@ -2,17 +2,42 @@ package routes
 
 import (
 	"github.com/2deadmen/domestic_backend/controllers"
-
+	_ "github.com/2deadmen/domestic_backend/docs" // Import the generated docs
 	"github.com/gin-gonic/gin"
+	files "github.com/swaggo/files" // Swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// InitRoutes godoc
+// @Summary Initialize API routes
+// @Description Sets up all the API routes, including Swagger documentation and user endpoints
+// @Tags Routes
 func InitRoutes() *gin.Engine {
 	router := gin.Default()
+
+	// Swagger route
+	// @Summary Swagger UI
+	// @Description Serve Swagger API documentation
+	// @Tags Documentation
+	// @Router /swagger/*any [get]
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(files.Handler))
 
 	// User routes
 	userGroup := router.Group("/users")
 	{
+		// @Summary Get all users
+		// @Description Retrieve a list of all users
+		// @Tags Users
+		// @Produce json
+		// @Router /users [get]
 		userGroup.GET("/", controllers.GetUsers)
+
+		// @Summary Create a new user
+		// @Description Add a new user to the system
+		// @Tags Users
+		// @Accept json
+		// @Produce json
+		// @Router /users [post]
 		userGroup.POST("/", controllers.CreateUser)
 	}
 
